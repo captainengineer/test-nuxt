1c2e47ac1df94f5cb835ddc3c1b40296f67cfdbb
Hiroshi Magome <yamorijakarta@gmail.com>
2023-12-26 17:39:53 +0900

    LINE風のチャット画面を実装した

--- pages/index.vue
+++ pages/index.vue
@@ -1,83 +1,91 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <div class="chat-container">
+    <header class="chat-header">
+      <h1>LINE風チャット</h1>
+    </header>
+    <div class="chat-messages">
+      <div v-for="(message, index) in messages" :key="index" :class="{'my-message': message.isMine, 'other-message': !message.isMine}">
+        <span>{{ message.text }}</span>
+      </div>
+    </div>
+    <div class="message-input">
+      <input type="text" v-model="newMessage" @keyup.enter="sendMessage" placeholder="メッセージを入力...">
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  data() {
+    return {
+      newMessage: '',
+      messages: [
+        { text: 'こんにちは！', isMine: false },
+        { text: 'こんにちは、元気ですか？', isMine: true }
+        // 他のメッセージ...
+      ]
+    };
+  },
+  methods: {
+    sendMessage() {
+      if (this.newMessage.trim() !== '') {
+        this.messages.push({ text: this.newMessage, isMine: true });
+        this.newMessage = '';
+      }
+    }
+  }
+};
 </script>
+
+<style>
+.chat-container {
+  max-width: 600px;
+  margin: auto;
+  border: 1px solid #ddd;
+  height: 90vh;
+  display: flex;
+  flex-direction: column;
+}
+
+.chat-header {
+  background-color: #00B900;
+  color: white;
+  text-align: center;
+  padding: 10px;
+}
+
+.chat-messages {
+  flex-grow: 1;
+  overflow-y: scroll;
+  padding: 10px;
+}
+
+.chat-messages div {
+  margin-bottom: 10px;
+  padding: 5px;
+  max-width: 70%;
+  word-break: break-word;
+}
+
+.my-message {
+  background-color: #DCF8C6;
+  margin-left: auto;
+  border-radius: 10px;
+}
+
+.other-message {
+  background-color: #FFFFFF;
+  border-radius: 10px;
+}
+
+.message-input {
+  padding: 10px;
+  background-color: #f4f4f4;
+}
+
+.message-input input {
+  width: 100%;
+  padding: 10px;
+  box-sizing: border-box;
+}
+</style>
